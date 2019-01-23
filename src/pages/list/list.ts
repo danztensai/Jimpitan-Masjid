6f44a54c57f4efcda4ce138c0dd726a1c55c76c6
Danial <danztensai@gmail.com>
2019-01-24 00:25:06 +0700

    Change Splash image and icon, Change Navigation header color into white

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -13,8 +13,7 @@ export class ListPage {
   items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    this.icons = ['list1','list2'];
 
     this.items = [];
     for(let i = 1; i < 11; i++) {
